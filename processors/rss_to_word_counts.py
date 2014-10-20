d5c915569effb3bc0b64af810c2b75fe07bc2ff6
qubey <qubey.dev@gmail.com>
2014-10-19 17:34:13 -0700

    complete rss parsing

--- processors/rss_to_word_counts.py
+++ processors/rss_to_word_counts.py
@@ -1,46 +1,67 @@
 #!/usr/bin/python
 
+import re
 import sys
 import time
+from datetime import date, timedelta
 from os import listdir, makedirs
 from os.path import isfile, join
 from xml.dom import minidom
 
-# read xml file
-# get all elements by tag name 'channel'
-# get all elements by tag name 'item'
-# item.title, item.description
+def countWords(sentence, countDict):
+    formatted = sentence.lower()
+    formatted = formatted[:formatted.find('<')]
+    formatted = re.sub('[.!,;]', '', formatted)
+    formatted = re.sub('\t', ' ', formatted)
+    formatted = formatted.strip()
+    words = formatted.split()
+    for word in words:
+        if word not in countDict.keys():
+            countDict[word] = 1
+        else:
+            countDict[word] = countDict[word] + 1
 
 input_dir = sys.argv[1]
 output_dir = sys.argv[2]
-out_file = time.strftime('%Y-%m-%d.rss')
+out_file_name = time.strftime('%Y-%m-%d.rss')
 
 directories = [ d for d in listdir(input_dir) if not isfile(join(input_dir, d))]
+
 for d in directories:
-    input_file = join(input_dir, d, out_file)
-    output_file = join(output_dir, d, out_file)
-    
+    output_file = join(output_dir, d, out_file_name)
     # make the output directory
     makedirs(name=join(output_dir, d), exist_ok=True)
 
-    rss_content = minidom.parse(input_file)
-    channels = rss_content.getElementsByTagName('channel')
+    # Aggregating over 30 days
+    for i in range(30):
+        in_file_date = date.today() - timedelta(days=i)
+        in_file_name = in_file_date.strftime('%Y-%m-%d.rss')
+        input_file = join(input_dir, d, in_file_name)
 
-    print("===================")
-    print(output_file)
-    print("===================")
-    for channel in channels:
-        items = channel.getElementsByTagName('item')
-        for item in items:
-            titleNodes = item.getElementsByTagName('title')
-            title = 'No title'
-            if (len(titleNodes) > 0):
-                title = titleNodes[0].firstChild.data
-            desc = 'No description'
-            descNodes = item.getElementsByTagName('description')
-            if (len(descNodes) > 0 and descNodes[0].firstChild is not None):
-                desc = descNodes[0].firstChild.data
+        if not isfile(input_file):
+            print("Can't find " + input_file)
+            continue
 
-            print("Title: " + title)
-            print("Desc: " + desc)
-            print("--")
+        word_counts = {}
+        rss_content = minidom.parse(input_file)
+        channels = rss_content.getElementsByTagName('channel')
+
+        for channel in channels:
+            items = channel.getElementsByTagName('item')
+            for item in items:
+                titleNodes = item.getElementsByTagName('title')
+                if (len(titleNodes) > 0):
+                    title = titleNodes[0].firstChild.data
+                    countWords(title, word_counts)
+
+                descNodes = item.getElementsByTagName('description')
+                if (len(descNodes) > 0 and descNodes[0].firstChild is not None):
+                    desc = descNodes[0].firstChild.data
+                    countWords(desc, word_counts)
+                
+
+    f = open(output_file, 'w')
+    for word in word_counts:
+        f.write(word + "\t" + str(word_counts[word]) + "\n")
+    f.close()
+    print("Wrote " + output_file)
---
+++ processors/run_today.sh
@@ -0,0 +1,2 @@
+#1/bin/bash
+python ~/news/processors/rss_to_word_counts.py ~/news/data/raw_feeds/ ~/news/data/word_counts
